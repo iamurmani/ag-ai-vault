bcff47249b1788fb3a049c0f5ce2727cd0b89e71
Manigandan <54367953+iamurmani@users.noreply.github.com>
2023-11-03 10:56:36 +0530

    added sign in page

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,10 +4,17 @@ import { AdminRoutingModule } from "./admin-routing.module";
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from "../material/materila.module";
 import { SharedModule } from "../shared/shared.module";
+import { AdminSignUpComponent } from "./components/admin-sign-up/admin-sign-up.component";
 
 @NgModule({
-    declarations:[AdminSignInComponent],
-    exports:[AdminSignInComponent],
+    declarations:[
+        AdminSignInComponent,
+        AdminSignUpComponent
+    ],
+    exports:[
+        AdminSignInComponent,
+        AdminSignUpComponent
+    ],
     imports:[
         CommonModule,
         AdminRoutingModule,
